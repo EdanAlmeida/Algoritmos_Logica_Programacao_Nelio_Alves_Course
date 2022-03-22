88d4930177ab5bcd8a4aece66b4d304755b348b6
EdanAlmeida <edluisalmeida@gmail.com>
2022-03-22 13:11:29 -0300

    new activities

--- problema_soma_impares.py
+++ problema_soma_impares.py
@@ -4,6 +4,17 @@ Leia 2 valores inteiros X e Y em qualquer ordem. A seguir, calcule e mostre a so
 """
 
 print('DIGITE DOIS NUMEROS INTEIROS: ')
-x = float(input(' '))
-y = float(input(' '))
+x = int(input(' '))
+y = int(input(' '))
 
+if x > y:
+    troca = x
+    x = y
+    y = troca
+
+sum = 0
+for i in range(x+1, y):
+    if i % 2 != 0:
+        sum = sum + i
+
+print(f'SOMA DOS IMPARES: {sum}')
---
+++ problema_soma_vetor.py
@@ -0,0 +1,25 @@
+"""
+Faça um programa que leia 'N' números reais e armazene-os em um vetor. Em seguida:
+    - imprimir todos os elementos do vetor;
+    - mostrar na tela a soma e a média dos elementos do vetor.
+
+"""
+
+n = int(input('Quantos numeros voce vai digitar? '))
+vet = [0 for x in range(n)]
+
+sum = 0
+for i in range(0, n):
+    vet[i] = float(input('Digite um numero: '))
+    sum = sum + vet[i]
+
+media = sum / n
+
+print()
+print('VALORES = ', end='')
+for i in range(0, n):
+    print(f'{vet[i]:.2f} ', end='')
+
+print()
+print(f'SOMA = {sum}')
+print(f'MEDIA = {media}')
